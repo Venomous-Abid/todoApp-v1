f48fe35979e657f9878f17eb5b72f2f0d4f4ef46
Abiduzzaman Abid <itisabidss@gmail.com>
2020-06-06 22:05:47 +0600

    Added Styles To the site

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var items =["Buy Food","Cook Food","Eat Food"];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", function(req,res){
     var today = new Date();
